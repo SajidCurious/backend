adc5c0fa29ac3b5c8706da3a4c2bbe8cbc868bca
Mohammed <sajidcurious9@gmail.com>
2024-01-16 15:45:43 +0530

    added figlet art and used it using node

--- script2.js
+++ script2.js
@@ -1,3 +1,14 @@
-let oneLinerJoke = require("one-liner-joke");
+// let oneLinerJoke = require("one-liner-joke");
 
-console.log(oneLinerJoke.getRandomJoke());
+// console.log(oneLinerJoke.getRandomJoke());
+
+var figlet = require("figlet");
+
+figlet("Mohammed Sajid", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
